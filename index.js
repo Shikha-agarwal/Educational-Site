4341551c87a67cb344895659b5fcf2a22935fb9a
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2022-02-02 19:35:26 +0530

    modified

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ jQuery(document).ready(function(){
     $('.navbar-collapse').on('click',function(){ 
         $('.navbar-collapse').collapse('hide');})
 
-    // jQuery(window).resize(function(){
-    //     if(jQuery(window).width()< "900px"){
-    //         jQuery(".navbar-text a").removeClass("signIn btn btn-sm me-2");
-    //     }
-    // });
+    $('#pass-status').click(function(){
+            var passInput = $('#passType');
+            var passStatus = $('#pass-status');
+            if(passInput.attr('type')=='password'){
+                passInput.attr("type",'text');
+                passStatus.removeClass('fa fa-eye');
+                passStatus.addClass('fa fa-eye-slash');
+            }
+            else
+            {
+              passInput.attr("type",'password');
+              passStatus.removeClass('fa fa-eye-slash');
+             passStatus.addClass('fa fa-eye');
+            }
+        })
 });
-
 
