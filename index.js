126985fb693e96870206170ff9883e368eb1dc42
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2022-02-03 17:39:22 +0530

    modified

--- index.js
+++ index.js
@@ -2,24 +2,33 @@ jQuery(document).ready(function(){
     $('.navbar-collapse').on('click',function(){ 
         $('.navbar-collapse').collapse('hide');})
 
-    $('#pass-status').click(function(){
-            var passInput = $('#passType');
-            var passStatus = $('#pass-status');
-            if(passInput.attr('type')=='password'){
-                passInput.attr("type",'text');
-                passStatus.removeClass('fa fa-eye');
-                passStatus.addClass('fa fa-eye-slash');
-            }
-            else
-            {
-              passInput.attr("type",'password');
-              passStatus.removeClass('fa fa-eye-slash');
-             passStatus.addClass('fa fa-eye');
-            }
-        })
-
-    $('.signin-button').click(function(){
-        $('form h2').text("Sign Up");
+    const loginForm = $('#login-form');
+    const createAccForm = $("#createAccount-form");
+    $('#linkcreateAccount').click(function(){
+        loginForm.addClass("form-hidden");
+        createAccForm.removeClass("form-hidden");
     })
+    $('#linklogin').click(function(){
+        loginForm.removeClass("form-hidden");
+        createAccForm.addClass("form-hidden");
+    })
+
+    $('.pass-status').click(function(){
+        var passInput = $('.passType');
+        var passStatus = $('.pass-status');
+        if(passInput.attr('type')=='password'){
+            passInput.attr("type",'text');
+            passStatus.removeClass('fa fa-eye');
+            passStatus.addClass('fa fa-eye-slash');
+        }
+        else
+        {
+          passInput.attr("type",'password');
+          passStatus.removeClass('fa fa-eye-slash');
+         passStatus.addClass('fa fa-eye');
+        }
+    })
+
+    $('.firstDiv').addClass("top-padding");
 });
 
